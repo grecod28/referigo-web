260c7a9a1f79304d3cd40918dc9e1bd0018495f5
Santiago <grecodominguezsantiago@gmail.com>
2026-08-28 21:51:52 +0200

    refactor: Ruta co layout main para incluir el header

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale } from "next-intl/server";
-import { Header } from "@/components/layout/header";
 import "../globals.css";
 
 const geistSans = Geist({
@@ -35,7 +34,6 @@ export default async function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <NextIntlClientProvider messages={messages} locale={locale}>
-          <Header />
           {children}
         </NextIntlClientProvider>
       </body>
